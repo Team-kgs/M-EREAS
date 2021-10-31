572743141c26aabe46a90bc460165c94bdb06a76
Edotnd <edotnd@gmail.com>
2021-10-31 22:01:11 +0900

    plus SkyViewCamera

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from ursina import *
 from ursina import texture
 from ursina import collider
 from ursina.prefabs.first_person_controller import FirstPersonController
+from SkyViewCamera import *
 
 class Voxel(Button):
     def __init__(self, position=(0,0,0)):
@@ -22,11 +23,7 @@ for z in range(8):
         voxel = Voxel((x*1.5, 0, z*1.5))
         voxel.rotation_y = 90
 
-camera = EditorCamera()
-camera.y = 3
-camera.rotation_y=90
-
-
+player = SkyViewCamera(y=2, origin_y=3)
 # ground = Entity(model='plane', scale=20, texture='white_cube', texture_scale=(20,20,20), collider='plane')
 
 app.run()
